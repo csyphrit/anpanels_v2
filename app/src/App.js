4d0a0ec5b6c7eb77b674bd1d1b6deb2152a107c2
Christie Jennings-Wyckoff <csyphrit@gmail.com>
2020-10-08 19:15:53 -0400

    Initial commit of React version of ANPanels

---
+++ app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">ANPanels</header>
+        </div>
+    );
+}
+
+export default App;
