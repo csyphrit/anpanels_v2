4bf63d6100f8ce39ba58509f8d17ed78c8bfae0d
csyphrit <csyphrit@gmail.com>
2022-01-05 20:07:33 -0500

    Move React front-end to app folder.

--- app/src/App.js
+++ app/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Anime North Panelist Registration v2
+        Anime North Panelist Registration
       </header>
     </div>
   );
